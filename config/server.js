943604e3b22683a3230411c54cef342252727627
ysaiascampos <ysaiascampos@gmail.com>
2020-12-28 21:42:16 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'acc7a12c795dee162135a3ad1ddd03f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2139b44c-3e37-4b99-a83d-0bd7108708e2'
+};
